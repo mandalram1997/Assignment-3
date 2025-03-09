9b2c875a6a2c4134939ee50bcec5d5562d5a75b0
SIBARAM MANDAL <mandalram.siba@gmail.com>
2025-03-09 17:19:29 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,8 @@
-
+n = int(input("Enter a Number : "))
+def recur_factorial(n):
+  if n == 1 or n == 0:
+    return 1
+  else:
+    return n * recur_factorial(n - 1)
+recur_factorial(5)
+print(f"Factorial of {n} is : {recur_factorial(5)}")
